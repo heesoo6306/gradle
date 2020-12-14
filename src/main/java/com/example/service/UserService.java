9fc5ae1606590e317ce6192ef88d33fa922d19a4
heesoo6306 <52159383+heesoo6306@users.noreply.github.com>
2020-12-14 15:28:07 +0900

    add : JPA and H2 database findAll , initialize 구현

---
+++ src/main/java/com/example/service/UserService.java
@@ -0,0 +1,48 @@
+package com.example.service;
+
+
+import com.example.model.User;
+import com.example.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class UserService {
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public void  findAll() {
+        for (User user : this.userRepository.findAll()) {
+            System.out.println(user);
+        }
+    }
+
+    public void initializeUsers() {
+        User user1 = User.builder()
+                .email("example1@sample.com")
+                .name("Mr. Example")
+                .phone("01000000000")
+                .build();
+
+        User user2 = User.builder()
+                .email("example2@sample.com")
+                .name("Mrs. Sample")
+                .phone("01000000000")
+                .build();
+
+        User user3 = User.builder()
+                .email("example3@sample.com")
+                .name("ms. Sample Data")
+                .phone("01000000000")
+                .build();
+
+        this.userRepository.save(user1);
+        this.userRepository.save(user2);
+        this.userRepository.save(user3);
+        this.userRepository.flush();
+    }
+}
