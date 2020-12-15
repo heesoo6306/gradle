cf2367c9ca6e5fad12b42f79f9758aab37ab9ee7
heesoo6306 <52159383+heesoo6306@users.noreply.github.com>
2020-12-15 10:18:25 +0900

    add : Sale and Product repository

--- src/main/java/com/example/service/UserService.java
+++ src/main/java/com/example/service/UserService.java
@@ -18,13 +18,9 @@ public class UserService {
         this.userRepository = userRepository;
     }
 
-    public User find(int userId) throws Exception{
+    public User find(int userId) throws Exception {
         Optional<User> searchedUser = this.userRepository.findById(userId);
-        if (searchedUser == null) {
-            throw  new Exception("해당 유저를 찾지 못하였습니다");
-        }
-
-        return searchedUser.get();
+        return searchedUser.orElseThrow(() -> new Exception("해당 유저를 찾지 못하였습니다"));
     }
 
     public List<User> findAll() {
